3bed07a3845c71532b1b46d5fa692ec33bff0565
nikhilsharma93 <nikhilsharma.22.93@gmail.com>
2017-11-27 17:41:36 -0500

    update

--- testRun.lua
+++ testRun.lua
@@ -1,5 +1,6 @@
 require 'rnn'
-require 'cunn'
+--require 'cunn'
+require 'lfs'
 
 EOS = "."
 GO = "!"
@@ -18,18 +19,12 @@ for loop_char = 1,#VOCAB do
     table.insert(reverse_vocab_table, c)
 end
 
-local function convertToInts(samples)
-    --print ('samples: ')
-    --print (samples)
-    for loop_samples = 1,#samples do
-        local s = samples[loop_samples]
-        local s_int = torch.Tensor(#s)
-        for loop_s = 1,#s do
-            s_int[loop_s] = vocab_table[s:sub(loop_s, loop_s)]
-        end
-        samples[loop_samples] = s_int
+local function convertToInts(seq)
+    local seq_int = torch.Tensor(#seq)
+    for loop_s = 1,#seq do
+        seq_int[loop_s] = vocab_table[seq:sub(loop_s, loop_s)]
     end
-    return samples
+    return seq_int
 end
 
 local function forwardConnect(encoder, decoder, seq_len)
@@ -67,17 +62,17 @@ local function maxProb(dec_out)
 end
 
 
-local function getOutput(enc_inp, dec_inp)
-    local current_enc_seq_len = enc_inp[1]:len()
-    enc_inp = convertToInts(enc_inp)[1]
-    --print ('size bef: '); print(enc_inp:size())
-    enc_inp = enc_inp:view(1,current_enc_seq_len)
-    --print ('size bef: '); print(enc_inp:size())
-    enc_inp = enc_inp:t()
+local function getOutput(enc_inp)
+    --enc_inp is a string
 
-    dec_inp = convertToInts(dec_inp)[1]
+    local current_enc_seq_len = enc_inp:len()
+    enc_inp = convertToInts(enc_inp)
+    --print ('size bef: '); print(enc_inp:size())
+    enc_inp = enc_inp:view(current_enc_seq_len,1)
+    --print ('size bef: '); print(enc_inp:size())
+
+    dec_inp = convertToInts(GO)
     dec_inp = dec_inp:view(1,1)
-    dec_inp = dec_inp:t()
 
     local enc_out = encoder:forward(enc_inp)
     --print ('encou: '); print(enc_out:size())
@@ -89,37 +84,51 @@ local function getOutput(enc_inp, dec_inp)
     local val, ind = torch.max(dec_out,3)
     --print ("val ind: ", val, ind[{1,1,1}])
     ind = ind[{1,1,1}]
-    out_str = reverse_vocab_table[ind]
-    print ('out_str... ', out_str)
+    local current_char = reverse_vocab_table[ind]
+    local out_str = current_char
+    --print ('out_str... ', out_str)
     loc = 1
-    while (out_str ~= EOS) do
-        print ('doing..')
-        --dec_inp = convertToInts({out_str})[1]
-        --dec_inp = dec_inp:view(1,1)
-        --dec_inp = dec_inp:t()
-        local temp_dec_in = target:sub(loc,loc)
-        temp_dec_in = vocab_table[temp_dec_in]
-        dec_inp:fill(temp_dec_in)
-        --dec_inp:fill(ind)
+    while (current_char ~= EOS) do
+        --print ('doing..')
+        --local temp_dec_in = target:sub(loc,loc)
+        --print (temp_dec_in)
+        --temp_dec_in = vocab_table[temp_dec_in]
+        --print (temp_dec_in)
+        --dec_inp:fill(temp_dec_in)
+        dec_inp:fill(ind)
         dec_out = decoder:forward(dec_inp)
-        local val, ind = torch.max(dec_out,3)
+        val, ind = torch.max(dec_out,3)
         ind = ind[{1,1,1}]
-        out_str = reverse_vocab_table[ind]
-        print ('out_str... ', out_str)
+
+        current_char = reverse_vocab_table[ind]
+
+        out_str = out_str..current_char
         loc = loc+1
     end
-
+    return out_str
 end
 
-enc_inp = {}
-enc_inp[1] = "print(911810)"
-dec_inp = {}
-dec_inp[1] = GO
-target = "print(915810)."
-decoder = torch.load('/home/nikhil/myCode/learning/Torch/learning_to_exc/git/learning_to_execute/results_5_2_500k_b128_r007/modelV1_decoder_epoch3.t7'):type('torch.DoubleTensor')
-encoder = torch.load('/home/nikhil/myCode/learning/Torch/learning_to_exc/git/learning_to_execute/results_5_2_500k_b128_r007/modelV1_encoder_epoch3.t7'):type('torch.DoubleTensor')
+local current_dir = lfs.currentdir()
+decoder = torch.load(current_dir..'/results_5_2_500k_b128_r007/modelV1_decoder_cpu.t7'):type('torch.DoubleTensor')
+encoder = torch.load(current_dir..'/results_5_2_500k_b128_r007/modelV1_encoder_cpu.t7'):type('torch.DoubleTensor')
 encoder:evaluate()
 decoder:evaluate()
---encoder:remember('both')
-decoder:remember('both')
-getOutput(enc_inp, dec_inp)
+
+inputs = {}
+targets = {}
+table.insert(inputs, "for x in range(4):-=18652")
+table.insert(inputs, "print((111 if 45<23 else 129))")
+--target = "print(10)."
+
+for loop_inp = 1, #inputs do
+    local enc_inp = inputs[loop_inp]
+    print ('\nINPUT:\n')
+    print (enc_inp)
+    encoder:remember('both')
+    decoder:remember('both')
+    local out_str = getOutput(enc_inp)
+    print ('OUTPUT:\n')
+    print (out_str)
+    encoder:forget()
+    decoder:forget()
+end
