1366470b06c9fa0775bc1ddc3eb4a0ae213ef713
nikhilsharma93 <nikhilsharma.22.93@gmail.com>
2017-11-26 22:03:16 -0500

    update

--- testRun.lua
+++ testRun.lua
@@ -1,113 +1,125 @@
---[[
-Example of "coupled" separate encoder and decoder networks, e.g. for sequence-to-sequence networks.
-]]--
-
 require 'rnn'
+require 'cunn'
 
-version = 1.4 -- Uses [get,set]GradHiddenState for LSTM
+EOS = "."
+GO = "!"
+VOCAB = "abcdefghijklmnopqrstuvwxyz <>:.!,;+=-\n*/()1234567890"
+VOCABSIZE = #VOCAB
 
-local opt = {}
-opt.learningRate = 0.1
-opt.hiddenSize = 6
-opt.numLayers = 1
-opt.useSeqLSTM = false -- faster implementation of LSTM + Sequencer
-opt.vocabSize = 7
-opt.seqLen = 7 -- length of the encoded sequence (with padding)
-opt.niter = 1000
+local vocab_table = {}
+for loop_char = 1,#VOCAB do
+    local c = VOCAB:sub(loop_char,loop_char)
+    vocab_table[c] = loop_char
+end
 
---[[ Forward coupling: Copy encoder cell and output to decoder LSTM ]]--
-function forwardConnect(enc, dec)
-   for i=1,#enc.lstmLayers do
-      if opt.useSeqLSTM then
-         dec.lstmLayers[i].userPrevOutput = enc.lstmLayers[i].output[opt.seqLen]
-         dec.lstmLayers[i].userPrevCell = enc.lstmLayers[i].cell[opt.seqLen]
-      else
-         dec.lstmLayers[i].userPrevOutput = nn.utils.recursiveCopy(dec.lstmLayers[i].userPrevOutput, enc.lstmLayers[i].outputs[opt.seqLen])
-         dec.lstmLayers[i].userPrevCell = nn.utils.recursiveCopy(dec.lstmLayers[i].userPrevCell, enc.lstmLayers[i].cells[opt.seqLen])
-      end
+local reverse_vocab_table = {}
+for loop_char = 1,#VOCAB do
+    local c = VOCAB:sub(loop_char,loop_char)
+    table.insert(reverse_vocab_table, c)
+end
+
+local function convertToInts(samples)
+    --print ('samples: ')
+    --print (samples)
+    for loop_samples = 1,#samples do
+        local s = samples[loop_samples]
+        local s_int = torch.Tensor(#s)
+        for loop_s = 1,#s do
+            s_int[loop_s] = vocab_table[s:sub(loop_s, loop_s)]
+        end
+        samples[loop_samples] = s_int
+    end
+    return samples
+end
+
+local function forwardConnect(encoder, decoder, seq_len)
+   for i=1,#encoder.lstm_layers do
+       decoder.lstm_layers[i].userPrevOutput = encoder.lstm_layers[i].output[seq_len]
+       decoder.lstm_layers[i].userPrevCell = encoder.lstm_layers[i].cell[seq_len]
    end
 end
 
---[[ Backward coupling: Copy decoder gradients to encoder LSTM ]]--
-function backwardConnect(enc, dec)
-   for i=1,#enc.lstmLayers do
-      if opt.useSeqLSTM then
-         enc.lstmLayers[i].userNextGradCell = dec.lstmLayers[i].userGradPrevCell
-         enc.lstmLayers[i].gradPrevOutput = dec.lstmLayers[i].userGradPrevOutput
-      else
-         enc:setGradHiddenState(opt.seqLen, dec:getGradHiddenState(0))
-      end
-   end
+local function maxProb(dec_out)
+    local dec_out = dec_out:clone():permute(2,1,3)
+    local out_str = "BATCH "..tostring(t).."\n"
+    for loop_samples = 1,1 do
+        local out = dec_out[loop_samples]
+        local dict = reverse_vocab_table
+
+        for i = 1,out:size(1) do
+            local val
+            local ind
+            val, ind = torch.max(out[i],1)
+            --val,ind=torch.sort(out[i])
+            --ind = torch.multinomial(out[i]:exp(),1)
+            local c = dict[ind[1]]
+            if c == EOS then
+                out_str = out_str..c
+                break
+            end
+            out_str = out_str..c
+        end
+
+        out_str = out_str.."\n\n"
+    end
+    out_str = out_str.."\n\n"
+    return out_str
 end
 
--- Encoder
-local enc = nn.Sequential()
-enc:add(nn.LookupTableMaskZero(opt.vocabSize, opt.hiddenSize))
-enc.lstmLayers = {}
-for i=1,opt.numLayers do
-   if opt.useSeqLSTM then
-      enc.lstmLayers[i] = nn.SeqLSTM(opt.hiddenSize, opt.hiddenSize)
-      enc.lstmLayers[i]:maskZero()
-      enc:add(enc.lstmLayers[i])
-   else
-      enc.lstmLayers[i] = nn.LSTM(opt.hiddenSize, opt.hiddenSize):maskZero(1)
-      enc:add(nn.Sequencer(enc.lstmLayers[i]))
-   end
+
+local function getOutput(enc_inp, dec_inp)
+    local current_enc_seq_len = enc_inp[1]:len()
+    enc_inp = convertToInts(enc_inp)[1]
+    --print ('size bef: '); print(enc_inp:size())
+    enc_inp = enc_inp:view(1,current_enc_seq_len)
+    --print ('size bef: '); print(enc_inp:size())
+    enc_inp = enc_inp:t()
+
+    dec_inp = convertToInts(dec_inp)[1]
+    dec_inp = dec_inp:view(1,1)
+    dec_inp = dec_inp:t()
+
+    local enc_out = encoder:forward(enc_inp)
+    --print ('encou: '); print(enc_out:size())
+    forwardConnect(encoder, decoder, current_enc_seq_len)
+    --print ('decin: '); print (dec_inp:size())
+    --print (dec_inp)
+    local dec_out = decoder:forward(dec_inp)
+    --print ('dec out: '); print (dec_out)
+    local val, ind = torch.max(dec_out,3)
+    --print ("val ind: ", val, ind[{1,1,1}])
+    ind = ind[{1,1,1}]
+    out_str = reverse_vocab_table[ind]
+    print ('out_str... ', out_str)
+    loc = 1
+    while (out_str ~= EOS) do
+        print ('doing..')
+        --dec_inp = convertToInts({out_str})[1]
+        --dec_inp = dec_inp:view(1,1)
+        --dec_inp = dec_inp:t()
+        local temp_dec_in = target:sub(loc,loc)
+        temp_dec_in = vocab_table[temp_dec_in]
+        dec_inp:fill(temp_dec_in)
+        --dec_inp:fill(ind)
+        dec_out = decoder:forward(dec_inp)
+        local val, ind = torch.max(dec_out,3)
+        ind = ind[{1,1,1}]
+        out_str = reverse_vocab_table[ind]
+        print ('out_str... ', out_str)
+        loc = loc+1
+    end
+
 end
-enc:add(nn.Select(1, -1))
 
--- Decoder
-local dec = nn.Sequential()
-dec:add(nn.LookupTableMaskZero(opt.vocabSize, opt.hiddenSize))
-dec.lstmLayers = {}
-for i=1,opt.numLayers do
-   if opt.useSeqLSTM then
-      dec.lstmLayers[i] = nn.SeqLSTM(opt.hiddenSize, opt.hiddenSize)
-      dec.lstmLayers[i]:maskZero()
-      dec:add(dec.lstmLayers[i])
-   else
-      dec.lstmLayers[i] = nn.LSTM(opt.hiddenSize, opt.hiddenSize):maskZero(1)
-      dec:add(nn.Sequencer(dec.lstmLayers[i]))
-   end
-end
-dec:add(nn.Sequencer(nn.MaskZero(nn.Linear(opt.hiddenSize, opt.vocabSize), 1)))
-dec:add(nn.Sequencer(nn.MaskZero(nn.LogSoftMax(), 1)))
-
-local criterion = nn.SequencerCriterion(nn.MaskZeroCriterion(nn.ClassNLLCriterion(),1))
-
--- Some example data (batchsize = 2) with variable length input and output sequences
-
--- The input sentences to the encoder, padded with zeros from the left
-local encInSeq = torch.Tensor({{0,0,0,0,1,2,3},{0,0,0,4,3,2,1}}):t()
--- The input sentences to the decoder, padded with zeros from the right.
--- Label '6' represents the start of a sentence (GO).
-local decInSeq = torch.Tensor({{6,1,2,3,4,0,0,0},{6,5,4,3,2,1,0,0}}):t()
-
--- The expected output from the decoder (it will return one character per time-step),
--- padded with zeros from the right
--- Label '7' represents the end of sentence (EOS).
-local decOutSeq = torch.Tensor({{1,2,3,4,7,0,0,0},{5,4,3,2,1,7,0,0}}):t()
-
-for i=1,opt.niter do
-   enc:zeroGradParameters()
-   dec:zeroGradParameters()
-
-   -- Forward pass
-   local encOut = enc:forward(encInSeq)
-   forwardConnect(enc, dec)
-   local decOut = dec:forward(decInSeq)
-
-   local err = criterion:forward(decOut, decOutSeq)
-
-   print(string.format("Iteration %d ; NLL err = %f ", i, err))
-
-   -- Backward pass
-   local gradOutput = criterion:backward(decOut, decOutSeq)
-   dec:backward(decInSeq, gradOutput)
-   backwardConnect(enc, dec)
-   local zeroTensor = torch.Tensor(encOut):zero()
-   enc:backward(encInSeq, zeroTensor)
-
-   dec:updateParameters(opt.learningRate)
-   enc:updateParameters(opt.learningRate)
-end
+enc_inp = {}
+enc_inp[1] = "print((6365 if 62722<17224 else 43665))"
+dec_inp = {}
+dec_inp[1] = GO
+target = "43665"
+decoder = torch.load('/home/nikhil/myCode/git/learning_to_execute/results_progeval_5_2_10k/modelV1_decoder.t7'):type('torch.DoubleTensor')
+encoder = torch.load('/home/nikhil/myCode/git/learning_to_execute/results_progeval_5_2_10k/modelV1_encoder.t7'):type('torch.DoubleTensor')
+encoder:evaluate()
+decoder:evaluate()
+--encoder:remember('both')
+decoder:remember('both')
+getOutput(enc_inp, dec_inp)
